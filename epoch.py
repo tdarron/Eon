e7650bac71b31c88e2b1dc9735ee23a00a7ed452
Thomas <tdarron@gmail.com>
2022-02-28 19:56:24 -0500

    cleaned up a bit and added loops

--- epoch.py
+++ epoch.py
@@ -1,6 +1,7 @@
 # !/bin/env python3
 
 import argparse
+import datetime
 
 """
 python3 epoch.py --times=
@@ -12,4 +13,16 @@ parser.add_argument('--times', '-t', type=str, help='Raw epoch time file that wi
 args = parser.parse_args()
 
 # Load passed file to list
-epoch_list = open(args.times,)
+with open(args.times) as t:
+    epoch_list = [int(stamp) for stamp in t]
+print(epoch_list)
+
+# Begin loop of converting
+try:
+    # Attempt in seconds first
+    for i in epoch_list:
+        print(datetime.datetime.fromtimestamp(i).strftime('%Y-%m-%d %H:%M:%S.%f'))
+except:
+    # Attempt milliseconds
+    for i in epoch_list:
+        print(datetime.datetime.fromtimestamp(i/1000.0).strftime('%Y-%m-%d %H:%M:%S.%f'))
